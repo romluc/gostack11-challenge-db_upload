9d173f8c4bc6bc557e3300af58cfad0a775cb74f
romluc <romulofilhofilho@gmail.com>
2020-05-11 11:48:52 -0300

    Create Transactions get route using Repository

--- src/routes/transactions.routes.ts
+++ src/routes/transactions.routes.ts
@@ -1,4 +1,5 @@
 import { Router } from 'express';
+import { getCustomRepository } from 'typeorm';
 
 import TransactionsRepository from '../repositories/TransactionsRepository';
 import CreateTransactionService from '../services/CreateTransactionService';
@@ -8,7 +9,11 @@ import CreateTransactionService from '../services/CreateTransactionService';
 const transactionsRouter = Router();
 
 transactionsRouter.get('/', async (request, response) => {
-  // TODO
+  const transactionsRepository = getCustomRepository(TransactionsRepository);
+
+  const transaction = await transactionsRepository.find();
+
+  return response.json(transaction);
 });
 
 transactionsRouter.post('/', async (request, response) => {
